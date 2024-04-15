d8ea1fee83b8c0c912ca4dc6c0e6eeb445b5b7b2
mrtuncer <148785051+mrtuncr@users.noreply.github.com>
2024-04-16 00:18:22 +0300

    Update Welcome.tsx

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,10 +1,8 @@
 export const Welcome: React.FC = () => (
   <div className="bg-white border-gray-100 border-2 rounded-lg px-8 py-5 mr-20 w-full">
-    <h1 className="text-2xl font-bold mb-2">🤖 Hello, I am Daneel</h1>
+    <h1 className="text-2xl font-bold mb-2">🤖 Merhaba, Ben Yapay zeka ile eğitilmiş bir diyetisyen robotuyum. </h1>
     <p>
-      You can use me to create your own chat bot app. In this demo I am an
-      expert in movie reviews, so feel free to ask me about movies! Then, create
-      your own bot in under five minutes.
+      Sağlıklı yaşam için gerekli, ihtiyaca yönelik diyet programlarını, beslenme önerilerini hazırlayabilir ve bunlara yönelik tüm sorularına cevap verebilirim. Aşağıdaki bazı hazır sorularla başlayabiliriz.
     </p>
     <p>
       <a className="underline" href="https://github.com/ascorbic/daneel#readme">
