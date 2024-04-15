92f3af5cc313a4cf30741f26904e9248523755c3
mrtuncer <148785051+mrtuncr@users.noreply.github.com>
2024-04-16 00:52:42 +0300

    Update Welcome.tsx

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,7 +1,6 @@
 export const Welcome: React.FC = () => (
   <div className="flex flex-col items-center bg-white border-gray-100 border-2 rounded-lg px-8 py-5 my-20 mr-20 w-full">
-    <h1 className="text-2xl font-bold mb-2"><p>🤖</p> 
-      <p>Merhaba, Ben Yapay zeka ile eğitilmiş bir diyetisyen robotuyum.</p> </h1>
+    <h1 className="text-2xl font-bold mb-2">🤖<p>Merhaba, Ben Yapay zeka ile eğitilmiş bir diyetisyen robotuyum.</p> </h1>
     <p>
       Sağlıklı yaşam için gerekli, ihtiyaca yönelik diyet programlarını, beslenme önerilerini hazırlayabilir ve bunlara yönelik tüm sorularına cevap verebilirim. 
     </p>
