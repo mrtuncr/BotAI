9ad7cba61aff70f866247756523d8f06def283c1
mrtuncer <148785051+mrtuncr@users.noreply.github.com>
2024-04-16 00:20:21 +0300

    Update Welcome.tsx

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -2,12 +2,10 @@ export const Welcome: React.FC = () => (
   <div className="bg-white border-gray-100 border-2 rounded-lg px-8 py-5 mr-20 w-full">
     <h1 className="text-2xl font-bold mb-2">🤖 Merhaba, Ben Yapay zeka ile eğitilmiş bir diyetisyen robotuyum. </h1>
     <p>
-      Sağlıklı yaşam için gerekli, ihtiyaca yönelik diyet programlarını, beslenme önerilerini hazırlayabilir ve bunlara yönelik tüm sorularına cevap verebilirim. Aşağıdaki bazı hazır sorularla başlayabiliriz.
+      Sağlıklı yaşam için gerekli, ihtiyaca yönelik diyet programlarını, beslenme önerilerini hazırlayabilir ve bunlara yönelik tüm sorularına cevap verebilirim. 
     </p>
     <p>
-      <a className="underline" href="https://github.com/ascorbic/daneel#readme">
-        Build your own
-      </a>
+     Aşağıdaki bazı hazır sorularla başlayabiliriz.
     </p>
   </div>
 );
